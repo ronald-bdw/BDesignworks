b3e3cea5b98f6921cfec33ef0c49591e60235d11
ellinakuznetcova <ellina.kuznetcova@flatstack.com>
2016-09-06 19:20:30 +0300

    remove redudand code

--- BDesignworks/TrialPageView.swift
+++ BDesignworks/TrialPageView.swift
@@ -11,16 +11,4 @@ import Foundation
 class TrialPageView: UIViewController {
     @IBOutlet weak var containerView: UIView!
     
-    
-//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-//        guard let identifier = segue.identifier, type = SegueType(identifier) else {return super.prepareForSegue(segue, sender: sender)}
-//        switch type {
-//        case .PageViewController:
-//            self.pageViewController = (segue.destinationViewController as! UIPageViewController)
-//            self.pageViewController?.dataSource = self
-//            self.pageViewController?.setViewControllers([PageControllers.First.controller], direction: .Forward, animated: false, completion: nil)
-////        default:
-////            break
-//        }
-//    }
 }
