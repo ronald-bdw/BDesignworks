0a5974860d952dbf42d8b4ab0c99163b2114e324
ellinakuznetcova <ellina.kuznetcova@flatstack.com>
2016-09-06 19:56:28 +0300

    added selection views

--- BDesignworks/TrialPageView.swift
+++ BDesignworks/TrialPageView.swift
@@ -10,5 +10,34 @@ import Foundation
 
 class TrialPageView: UIViewController {
     @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var firstViewSelectionView: TrialPageSelectionView!
+    @IBOutlet weak var secondViewSelectionView: TrialPageSelectionView!
+    @IBOutlet weak var thirdViewSelectionView: TrialPageSelectionView!
+    @IBOutlet weak var fourthViewSelectionView: TrialPageSelectionView!
     
+    private var selectionViews: [TrialPageSelectionView] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectionViews = [self.firstViewSelectionView, self.secondViewSelectionView, self.thirdViewSelectionView, self.fourthViewSelectionView]
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let destinationViewController = segue.destinationViewController
+        for view in destinationViewController.view.subviews {
+            if let scrollView = view as? UIScrollView {
+                scrollView.delegate = self
+                return
+            }
+        }
+    }
+}
+
+extension TrialPageView: UIScrollViewDelegate {
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let page = Int(scrollView.contentOffset.x / self.containerView.fs_width)
+        
+        self.selectionViews.forEach({$0.selected = false})
+        self.selectionViews[page].selected = true
+    }
 }
