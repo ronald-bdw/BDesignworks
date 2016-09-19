c3b32f951b3a5cfbd2dfa26d0999e1046a4b5843
ellinakuznetcova <ellina.kuznetcova@flatstack.com>
2016-09-19 11:58:03 +0300

    enabled background delivery

--- BDesignworks/HealthKitManager.swift
+++ BDesignworks/HealthKitManager.swift
@@ -33,11 +33,9 @@ class HealthKitManager
     func sendHealthKitData() {
         let dataTypesToRead = NSSet(objects: self.stepsCount)
 //        let itemsToWrite = Set(arrayLiteral: HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)!)
-        self.healthStore?.requestAuthorizationToShareTypes(nil, readTypes: dataTypesToRead as? Set<HKObjectType>, completion: { [unowned self] (success, error) in
+        self.healthStore?.requestAuthorizationToShareTypes(nil, readTypes: dataTypesToRead as? Set<HKObjectType>, completion: { [weak self] (success, error) in
             if success {
-//                self.saveSteps()
-//                self.querySteps()
-                self.enableBackgroundDelivery()
+                self?.enableBackgroundDelivery()
             } else {
                 Logger.error("\(error)")
             }
@@ -45,23 +43,9 @@ class HealthKitManager
     }
     
     func enableBackgroundDelivery() {
-        self.healthStore?.enableBackgroundDeliveryForType(self.stepsCount, frequency: .Hourly, withCompletion: { (success, error) in
-            guard error == nil else {
-                
-                let notification = UILocalNotification()
-                notification.alertBody = error?.description
-                notification.fireDate = NSDate()
-                notification.timeZone = NSTimeZone.defaultTimeZone()
-                UIApplication.sharedApplication().scheduleLocalNotification(notification)
-                
-                print(error); return}
-            
-            let notification = UILocalNotification()
-            notification.alertBody = "background delivery enabled completion"
-            notification.fireDate = NSDate()
-            notification.timeZone = NSTimeZone.defaultTimeZone()
-            UIApplication.sharedApplication().scheduleLocalNotification(notification)
-            self.queryStepsInBackground()
+        self.healthStore?.enableBackgroundDeliveryForType(self.stepsCount, frequency: .Hourly, withCompletion: { [weak self] (success, error) in
+            guard error == nil else { Logger.error(error); return}
+            self?.queryStepsInBackground()
         })
     }
     
@@ -81,66 +65,42 @@ class HealthKitManager
     
     func querySteps(completionHandler: HKObserverQueryCompletionHandler? = nil) {
         
-        
-        let notification = UILocalNotification()
-        notification.alertBody = "query steps entered"
-        notification.fireDate = NSDate()
-        notification.timeZone = NSTimeZone.defaultTimeZone()
-        UIApplication.sharedApplication().scheduleLocalNotification(notification)
-        
         let sampleQuery = HKSampleQuery(sampleType: self.stepsCount,
                                         predicate: nil,
                                         limit: self.defaultSamplesCount,
                                         sortDescriptors: nil)
-        {  (query, results, error) in
-            
-            
-            let notification1 = UILocalNotification()
-            notification.alertBody = "query steps completion"
-            notification.fireDate = NSDate()
-            notification.timeZone = NSTimeZone.defaultTimeZone()
-            UIApplication.sharedApplication().scheduleLocalNotification(notification1)
+        {  [weak self] (query, results, error) in
             
             guard let results = results as? [HKQuantitySample] else {return}
+            guard let sself = self else {return}
+            let endDate = NSDate()
+            let startDate = User.getMainUser()?.lastStepsHealthKitUpdateDate ?? endDate.fs_dateByAddingDays(-sself.defaultDaysToStepsCount)
             
-//            let endDate = NSDate()
-//            let startDate = User.getMainUser()?.lastStepsHealthKitUpdateDate ?? endDate.fs_dateByAddingDays(-self.defaultDaysToStepsCount)
-            
-//            let validResults = results.filter({startDate.compare($0.startDate) == .OrderedAscending})
-            let steps = results.map({ENSteps(startDate: $0.startDate, finishDate: $0.endDate, count: Int($0.quantity.doubleValueForUnit((HKUnit.countUnit()))))})
+            let validResults = results.filter({startDate.compare($0.startDate) == .OrderedAscending})
+            let steps = validResults.map({ENSteps(startDate: $0.startDate, finishDate: $0.endDate, count: Int($0.quantity.doubleValueForUnit((HKUnit.countUnit()))))})
             guard steps.count > 0 else {return}
             
-            var text = "steps count: "
-            for step in steps {
-                text += "\(step.count) "
-            }
-            Logger.debug(text)
-            
-            let notification = UILocalNotification()
-            notification.alertBody = text
-            notification.fireDate = NSDate()
-            notification.timeZone = NSTimeZone.defaultTimeZone()
-            UIApplication.sharedApplication().scheduleLocalNotification(notification)
-            completionHandler?()
-//            Router.Steps.Send(steps: steps, source: .HealthKit).request().responseObject({ (response: Response<RTStepsSendResponse, RTError>) in
-//                switch response.result {
-//                case .Success(_):
-//                    do {
-//                        let realm = try Realm()
-//                        let user = realm.objects(User).first
-//                        try realm.write({
-//                            user?.lastStepsHealthKitUpdateDate = endDate
-//                        })
-//                        Logger.debug("updatedStepsDate: \(user?.lastStepsHealthKitUpdateDate)")
-//                    }
-//                    catch let error {
-//                        Logger.error("\(error)")
-//                    }
-//                case .Failure(let error):
-//                    Logger.error("\(error)")
-//                }
-//            })}
-            }
+            Router.Steps.Send(steps: steps, source: .HealthKit).request().responseObject({ (response: Response<RTStepsSendResponse, RTError>) in
+                switch response.result {
+                case .Success(_):
+                    do {
+                        let realm = try Realm()
+                        let user = realm.objects(User).first
+                        try realm.write({
+                            user?.lastStepsHealthKitUpdateDate = endDate
+                        })
+                        Logger.debug("updatedStepsDate: \(user?.lastStepsHealthKitUpdateDate)")
+                    }
+                    catch let error {
+                        Logger.error("\(error)")
+                        
+                    }
+                case .Failure(let error):
+                    Logger.error("\(error)")
+                }
+                
+                completionHandler?()
+            })}
         
         self.healthStore?.executeQuery(sampleQuery)
     }
@@ -148,20 +108,7 @@ class HealthKitManager
     func queryStepsInBackground() {
         let observerQuery = HKObserverQuery(sampleType: self.stepsCount, predicate: nil) { [weak self] (query, completionHandler, error) in
             
-            guard error == nil else {
-                
-                let notification = UILocalNotification()
-                notification.alertBody = error?.description
-                notification.fireDate = NSDate()
-                notification.timeZone = NSTimeZone.defaultTimeZone()
-                UIApplication.sharedApplication().scheduleLocalNotification(notification)
-                Logger.error("\(error)"); return}
-            
-            let notification = UILocalNotification()
-            notification.alertBody = "observer query"
-            notification.fireDate = NSDate()
-            notification.timeZone = NSTimeZone.defaultTimeZone()
-            UIApplication.sharedApplication().scheduleLocalNotification(notification)
+            guard error == nil else { Logger.error(error); return}
             
             self?.querySteps(completionHandler)
         }
