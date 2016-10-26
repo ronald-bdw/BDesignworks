c065257d154296e5ffb1e4088f00d781a0c1f55a
Ильдар Залялов <Zalyalov.Ild@gmail.com>
2016-10-26 19:14:04 +0300

    add comment for future

--- BDesignworks/TrialPageContainer.swift
+++ BDesignworks/TrialPageContainer.swift
@@ -14,8 +14,7 @@ class TrialPageContainer: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //if current device is iPhone5 and lower
         guard self.view.bounds.height < CGFloat(570) else {return}
         for view in self.containerView.subviews{
             for containsView in view.subviews{
@@ -28,21 +27,5 @@ class TrialPageContainer: UIViewController {
             
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
