129a0e535c705f3748ad95d33355f4f304c9cccd
ellinakuznetcova <ellina.kuznetcova@flatstack.com>
2016-11-07 16:16:57 +0300

    added receipt validation

---
+++ BDesignworks/RegistrationFooter.swift
@@ -0,0 +1,13 @@
+//
+//  RegistrationFooter.swift
+//  BDesignworks
+//
+//  Created by Ellina Kuznetcova on 08/11/2016.
+//  Copyright © 2016 Flatstack. All rights reserved.
+//
+
+
+class RegistrationFooter: UITableViewCell {
+    
+    @IBOutlet weak var submitButton: UIButton!
+}
