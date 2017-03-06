41f8feab6d0b3161fa21133f8be682ee6c52561f
ellinakuznetcova <ellina.kuznetcova@flatstack.com>
2017-03-06 12:35:52 +0300

    fixed inapp status sending

--- BDesignworks/InAppManager.swift
+++ BDesignworks/InAppManager.swift
@@ -153,8 +153,7 @@ class InAppManager: NSObject {
     }
     
     func sendStatus() {
-        guard ENUser.getMainUser()?.isSubscriber == true,
-            let lIsTrialPurchased = self.isTrialPurchased,
+        guard let lIsTrialPurchased = self.isTrialPurchased,
             let lExpiration = self.expirationDate,
             let lPurchasedProduct = self.purchasedProduct else {return}
         let plan = lIsTrialPurchased ?
